c2e842559d919f59320f6f7bd85e9b24555bdf99
Marcus Ball <marcus@marcusball.me>
2016-03-21 20:53:43 -0400

    Auto-indented request test file.

--- tests/requesttest.php
+++ tests/requesttest.php
@@ -13,7 +13,7 @@ class RequestTest extends \PHPUnit_Framework_TestCase{
     $this->assertEquals(Request::parsePath('/index.php','\\',false),'/index.php'); //Windows
   }
 
-public function testParsePathRootWithParams(){
+  public function testParsePathRootWithParams(){
     //Request to root, '/', no filename, with query params stripped
     $this->assertEquals(Request::parsePath('/?test=true&false','/',false),'/');
     $this->assertEquals(Request::parsePath('/?test=true&false','\\',false),'/'); //Windows
@@ -44,195 +44,195 @@ public function testParsePathRootWithParams(){
   }
 
   public function testParsePathSubdirectoryWithParams(){
-      //Request to root, '/', no filename, with query params stripped
-      $this->assertEquals(Request::parsePath('/subdirectory/?test=true&false','/subdirectory',false),'/');
+    //Request to root, '/', no filename, with query params stripped
+    $this->assertEquals(Request::parsePath('/subdirectory/?test=true&false','/subdirectory',false),'/');
 
-      //Request to root, '/', no filename, with query params preserved
-      $this->assertEquals(Request::parsePath('/subdirectory/?test=true&false','/subdirectory',true),'/?test=true&false');
+    //Request to root, '/', no filename, with query params preserved
+    $this->assertEquals(Request::parsePath('/subdirectory/?test=true&false','/subdirectory',true),'/?test=true&false');
 
-      //Request to root index file, with query params stripped
-      $this->assertEquals(Request::parsePath('/subdirectory/index.php?test=true&false','/subdirectory',false),'/index.php');
+    //Request to root index file, with query params stripped
+    $this->assertEquals(Request::parsePath('/subdirectory/index.php?test=true&false','/subdirectory',false),'/index.php');
 
-      //Request to root index file, with query params preserved
-      $this->assertEquals(Request::parsePath('/subdirectory/index.php?test=true&false','/subdirectory',true),'/index.php?test=true&false');
-    }
+    //Request to root index file, with query params preserved
+    $this->assertEquals(Request::parsePath('/subdirectory/index.php?test=true&false','/subdirectory',true),'/index.php?test=true&false');
+  }
 
-    public function testCleanPathRoot(){
-      $this->assertEquals(Request::cleanPath(''), 'index');
-      $this->assertEquals(Request::cleanPath('/'), 'index');
+  public function testCleanPathRoot(){
+    $this->assertEquals(Request::cleanPath(''), 'index');
+    $this->assertEquals(Request::cleanPath('/'), 'index');
 
-      $this->assertEquals(Request::cleanPath('/index.php'), 'index'); //Needs to be here in case Config::core('request_php_extension') is != '.php'
+    $this->assertEquals(Request::cleanPath('/index.php'), 'index'); //Needs to be here in case Config::core('request_php_extension') is != '.php'
 
-      $this->assertEquals(Request::cleanPath('/index'.Config::core('request_php_extension')), 'index');
+    $this->assertEquals(Request::cleanPath('/index'.Config::core('request_php_extension')), 'index');
 
-      $this->assertEquals(Request::cleanPath('/index'.Config::core('request_php_extension').'?test=true&false'), 'index');
-    }
+    $this->assertEquals(Request::cleanPath('/index'.Config::core('request_php_extension').'?test=true&false'), 'index');
+  }
 
-    public function testDirectory(){
-      $this->assertEquals(Request::cleanPath('/subdirectory/'),'subdirectory/index');
+  public function testDirectory(){
+    $this->assertEquals(Request::cleanPath('/subdirectory/'),'subdirectory/index');
 
-      $this->assertEquals(Request::cleanPath('/subdirectory/index'.Config::core('request_php_extension')),'subdirectory/index');
+    $this->assertEquals(Request::cleanPath('/subdirectory/index'.Config::core('request_php_extension')),'subdirectory/index');
 
-      $this->assertEquals(Request::cleanPath('/subdirectory/test'.Config::core('request_php_extension')),'subdirectory/test');
+    $this->assertEquals(Request::cleanPath('/subdirectory/test'.Config::core('request_php_extension')),'subdirectory/test');
 
-      $this->assertEquals(Request::cleanPath('/subdirectory/nested/'),'subdirectory/nested/index');
-    }
+    $this->assertEquals(Request::cleanPath('/subdirectory/nested/'),'subdirectory/nested/index');
+  }
 
-    public function testRequestIndex(){
-      //Simple tests of requesting the index page
-      {
-        $request = Request::createRequest('index.php', '/', false);
-        $this->assertEquals($request->getPath(), 'index');
-      }
-      {
-        $request = Request::createRequest('/', '/', false);
-        $this->assertEquals($request->getPath(), 'index');
-      }
-    }
-
-    public function testSetMethod(){
-      $makeRequestWithAndGetMethod = function($method){
-        $request = Request::createRequest('index.php', '/', false);
-        $request->setMethod($method);
-        return $request->getMethod();
-      };
-
+  public function testRequestIndex(){
+    //Simple tests of requesting the index page
+    {
       $request = Request::createRequest('index.php', '/', false);
-      $this->assertEquals(RequestMethod::GET, $request->getMethod());
-
-      $this->assertEquals($makeRequestWithAndGetMethod('GET'), RequestMethod::GET);
-      $this->assertEquals($makeRequestWithAndGetMethod('get'), RequestMethod::GET);
-      $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::GET), RequestMethod::GET);
-
-      $this->assertEquals($makeRequestWithAndGetMethod('POST'), RequestMethod::POST);
-      $this->assertEquals($makeRequestWithAndGetMethod('post'), RequestMethod::POST);
-      $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::POST), RequestMethod::POST);
-
-      $this->assertEquals($makeRequestWithAndGetMethod('PUT'), RequestMethod::PUT);
-      $this->assertEquals($makeRequestWithAndGetMethod('put'), RequestMethod::PUT);
-      $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::PUT), RequestMethod::PUT);
-
-      $this->assertEquals($makeRequestWithAndGetMethod('PATCH'), RequestMethod::PUT);
-      $this->assertEquals($makeRequestWithAndGetMethod('patch'), RequestMethod::PUT);
-
-      $this->assertEquals($makeRequestWithAndGetMethod('DELETE'), RequestMethod::DELETE);
-      $this->assertEquals($makeRequestWithAndGetMethod('delete'), RequestMethod::DELETE);
-      $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::DELETE), RequestMethod::DELETE);
+      $this->assertEquals($request->getPath(), 'index');
     }
-
-    /**
-     * @expectedException InvalidArgumentException
-     */
-    public function testInvalidSetMethod(){
-      $makeRequestWithMethod = function($method){
-        $request = Request::createRequest('index.php', '/', false);
-        $request->setMethod($method);
-        return $request;
-      };
-
-      $makeRequestWithMethod('head');
-      $makeRequestWithMethod(100);
+    {
+      $request = Request::createRequest('/', '/', false);
+      $this->assertEquals($request->getPath(), 'index');
     }
+  }
 
-    public function testValidUrlRewrite(){
-      $rewriteRules = array(
-        //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
-        'test/file.php' => '^/some/other$'
-      );
+  public function testSetMethod(){
+    $makeRequestWithAndGetMethod = function($method){
+      $request = Request::createRequest('index.php', '/', false);
+      $request->setMethod($method);
+      return $request->getMethod();
+    };
 
-      Config::set(array('rewriterules'), $rewriteRules);
+    $request = Request::createRequest('index.php', '/', false);
+    $this->assertEquals(RequestMethod::GET, $request->getMethod());
 
-      //Function to get the parsed file from a request url
-      $getUrlFilePath = function($requestUrl){
-        $request = Request::createRequest($requestUrl, '/', true);
-        return $request->getPath();
-      };
+    $this->assertEquals($makeRequestWithAndGetMethod('GET'), RequestMethod::GET);
+    $this->assertEquals($makeRequestWithAndGetMethod('get'), RequestMethod::GET);
+    $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::GET), RequestMethod::GET);
 
-      $this->assertEquals($getUrlFilePath('/some/other'), 'test/file');
-      $this->assertEquals($getUrlFilePath('/some/other/'), 'test/file');
-      $this->assertEquals($getUrlFilePath('/test/file.php'), 'test/file');
+    $this->assertEquals($makeRequestWithAndGetMethod('POST'), RequestMethod::POST);
+    $this->assertEquals($makeRequestWithAndGetMethod('post'), RequestMethod::POST);
+    $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::POST), RequestMethod::POST);
 
-      $this->assertNotEquals($getUrlFilePath('/some/other.php'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/some/other.lol'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/some/other.ph'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/some/othertest'), 'test/file');
+    $this->assertEquals($makeRequestWithAndGetMethod('PUT'), RequestMethod::PUT);
+    $this->assertEquals($makeRequestWithAndGetMethod('put'), RequestMethod::PUT);
+    $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::PUT), RequestMethod::PUT);
 
-      $this->assertNotEquals($getUrlFilePath('/test/file.lol'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/test/file.ph'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/test/filetest'), 'test/file');
-    }
+    $this->assertEquals($makeRequestWithAndGetMethod('PATCH'), RequestMethod::PUT);
+    $this->assertEquals($makeRequestWithAndGetMethod('patch'), RequestMethod::PUT);
 
-    public function testInvalidUrlRewriteSameBase(){
-      $rewriteRules = array(
-        //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
-        'test/file.php' => '^/some/other$'
-      );
+    $this->assertEquals($makeRequestWithAndGetMethod('DELETE'), RequestMethod::DELETE);
+    $this->assertEquals($makeRequestWithAndGetMethod('delete'), RequestMethod::DELETE);
+    $this->assertEquals($makeRequestWithAndGetMethod(RequestMethod::DELETE), RequestMethod::DELETE);
+  }
 
-      Config::set(array('rewriterules'), $rewriteRules);
+  /**
+  * @expectedException InvalidArgumentException
+  */
+  public function testInvalidSetMethod(){
+    $makeRequestWithMethod = function($method){
+      $request = Request::createRequest('index.php', '/', false);
+      $request->setMethod($method);
+      return $request;
+    };
 
-      //Function to get the parsed file from a request url
-      $getUrlFilePath = function($requestUrl){
-        $request = Request::createRequest($requestUrl, '/', true);
-        return $request->getPath();
-      };
+    $makeRequestWithMethod('head');
+    $makeRequestWithMethod(100);
+  }
 
-      $this->assertNotEquals($getUrlFilePath('/some/other.php'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/some/other.lol'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/some/other.ph'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/some/othertest'), 'test/file');
+  public function testValidUrlRewrite(){
+    $rewriteRules = array(
+      //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
+      'test/file.php' => '^/some/other$'
+    );
 
-      $this->assertNotEquals($getUrlFilePath('/test/file.lol'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/test/file.ph'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/test/filetest'), 'test/file');
-    }
+    Config::set(array('rewriterules'), $rewriteRules);
 
-    public function testInvalidUrlRewriteFileBase(){
-      $rewriteRules = array(
-        //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
-        'test/file.php' => '^/some/other$'
-      );
+    //Function to get the parsed file from a request url
+    $getUrlFilePath = function($requestUrl){
+      $request = Request::createRequest($requestUrl, '/', true);
+      return $request->getPath();
+    };
 
-      Config::set(array('rewriterules'), $rewriteRules);
+    $this->assertEquals($getUrlFilePath('/some/other'), 'test/file');
+    $this->assertEquals($getUrlFilePath('/some/other/'), 'test/file');
+    $this->assertEquals($getUrlFilePath('/test/file.php'), 'test/file');
 
-      //Function to get the parsed file from a request url
-      $getUrlFilePath = function($requestUrl){
-        $request = Request::createRequest($requestUrl, '/', true);
-        return $request->getPath();
-      };
+    $this->assertNotEquals($getUrlFilePath('/some/other.php'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/some/other.lol'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/some/other.ph'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/some/othertest'), 'test/file');
 
-      $this->assertNotEquals($getUrlFilePath('/test/file.lol'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/test/file.ph'), 'test/file');
-      $this->assertNotEquals($getUrlFilePath('/test/filetest'), 'test/file');
-    }
+    $this->assertNotEquals($getUrlFilePath('/test/file.lol'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/test/file.ph'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/test/filetest'), 'test/file');
+  }
 
-    public function testValidUrlRewriteForced(){
-      $rewriteRules = array(
-        //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
-        'test/file.php' => '^/some/other$'
-      );
+  public function testInvalidUrlRewriteSameBase(){
+    $rewriteRules = array(
+      //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
+      'test/file.php' => '^/some/other$'
+    );
 
-      Config::set(array('rewriterules'), $rewriteRules);
+    Config::set(array('rewriterules'), $rewriteRules);
 
-      //Function to get the parsed file from a request url
-      $getUrlFilePath = function($requestUrl){
-        $request = Request::createRequest($requestUrl, '/', true, true);
-        return $request->getPath();
-      };
+    //Function to get the parsed file from a request url
+    $getUrlFilePath = function($requestUrl){
+      $request = Request::createRequest($requestUrl, '/', true);
+      return $request->getPath();
+    };
 
-      $this->assertEquals($getUrlFilePath('/some/other'), 'test/file');
-      $this->assertEquals($getUrlFilePath('/some/other/'), 'test/file');
-    }
+    $this->assertNotEquals($getUrlFilePath('/some/other.php'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/some/other.lol'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/some/other.ph'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/some/othertest'), 'test/file');
 
-    public function testInvalidUrlRewriteForced(){
-      $rewriteRules = array(
-        //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
-        'test/file.php' => '^/some/other$'
-      );
+    $this->assertNotEquals($getUrlFilePath('/test/file.lol'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/test/file.ph'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/test/filetest'), 'test/file');
+  }
 
-      Config::set(array('rewriterules'), $rewriteRules);
+  public function testInvalidUrlRewriteFileBase(){
+    $rewriteRules = array(
+      //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
+      'test/file.php' => '^/some/other$'
+    );
+
+    Config::set(array('rewriterules'), $rewriteRules);
+
+    //Function to get the parsed file from a request url
+    $getUrlFilePath = function($requestUrl){
+      $request = Request::createRequest($requestUrl, '/', true);
+      return $request->getPath();
+    };
+
+    $this->assertNotEquals($getUrlFilePath('/test/file.lol'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/test/file.ph'), 'test/file');
+    $this->assertNotEquals($getUrlFilePath('/test/filetest'), 'test/file');
+  }
+
+  public function testValidUrlRewriteForced(){
+    $rewriteRules = array(
+      //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
+      'test/file.php' => '^/some/other$'
+    );
+
+    Config::set(array('rewriterules'), $rewriteRules);
+
+    //Function to get the parsed file from a request url
+    $getUrlFilePath = function($requestUrl){
+      $request = Request::createRequest($requestUrl, '/', true, true);
+      return $request->getPath();
+    };
+
+    $this->assertEquals($getUrlFilePath('/some/other'), 'test/file');
+    $this->assertEquals($getUrlFilePath('/some/other/'), 'test/file');
+  }
+
+  public function testInvalidUrlRewriteForced(){
+    $rewriteRules = array(
+      //Test the ability to navigate to site.com/some/other and be handled by 'test/file.php'
+      'test/file.php' => '^/some/other$'
+    );
+
+    Config::set(array('rewriterules'), $rewriteRules);
 
 
-      $request = Request::createRequest('/test/file.php', '/', true, true);
-      $this->assertFalse($request); //Noi found (because direct access is disallowed)
-    }
+    $request = Request::createRequest('/test/file.php', '/', true, true);
+    $this->assertFalse($request); //Noi found (because direct access is disallowed)
+  }
 } ?>
